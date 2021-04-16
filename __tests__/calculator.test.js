639a2ac202dcec5b9ce7875667a2d95375ac6f15
Thomas Russell <t.p.russell19@gmail.com>
2021-04-16 10:41:45 -0700

    add failing test to calculate mercuryAge

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,9 +1,18 @@
 import Calculator from './../src/js/Calculator.js'
 describe("Calculator", () => {
+  let person;
+  beforeEach(() => {
+    person = new Calculator(50, "male", "USA")
+  });
+
   test("should correctly construct a calculator object based on a user's inputted age", () => {
-    const person = new Calculator(50, "male", "USA")
     expect(person.age).toEqual(50)
     expect(person.gender).toEqual("male")
     expect(person.country).toEqual("USA")
   })
+
+  test("should correctly calculate the age of a person in Mercury years", () => {
+    expect(person.mercuryAge).toEqual(208.33)
+  })
+
 })
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,4 +4,9 @@ export default class Calculator {
     this.gender = gender
     this.country = country
   }
+
+  mercuryCalc() {
+    
+  }
+
 }
