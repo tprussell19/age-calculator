3911b2f4daaf71068d1600db3c3aad0b148bd6a1
Thomas Russell <t.p.russell19@gmail.com>
2021-04-18 15:54:45 -0700

    add passing code for a person older than life expectancy on earth

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -66,7 +66,10 @@ export default class Calculator {
       this.earthReturn = "Wow! You have exactly reached the average life expectancy for a person in your demographic categories!"
       return this.earthReturn
     } else {
-
+      this.earthReturn;
+      this.earthYearsLeft = -this.earthYearsLeft
+      this.earthReturn = "Congrats! You have already passed the average life expectancy for a person in your demographic categories on Earth by " + `${this.earthYearsLeft}` + " years!"
+      return this.earthReturn
     }
   }
 
